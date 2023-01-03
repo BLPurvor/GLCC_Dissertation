6f44061dbc05de19673b026a8e710a291491ab40
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-01-03 19:36:03 +0000

    Update index.tsx
    Added Loading component designed to show the user a spinner if the page they have selected is not loaded at that time.
    
    Moved React based styling to logged out button page to fit in with AB testing results.
    
    Removed Register button as Auth0 does not handle registration through a different api call.
    
    Started creation of "dashboard" page to be displayed when the user is logged in.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,33 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import loStyles from "../styles/Landing.module.scss";
+import liStyles from "../styles/Home.module.scss";
 
-import Logo from "../assets/logo.png";
+import Header from "../components/";
+
+import imgLogo from "../assets/brand/logo.png";
+import imgLogin from "../assets/shared/login.svg";
+import imgLoading from "../assets/shared/loading.svg";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) {
-    return <div>Loading</div>;
+    return (
+      <div className={loStyles.loading}>
+        <Image
+          className={loStyles.logo}
+          src={imgLogo}
+          alt="Gregson Lane Cricket Club Logo"
+        />
+        <Image
+          className={loStyles.spinner}
+          src={imgLoading}
+          alt="Loading spinner"
+        />
+      </div>
+    );
   }
 
   if (error) {
@@ -22,23 +36,35 @@ export default function Home() {
 
   if (user) {
     return (
-      <>
-        <h1>Hello, {user.nickname}!</h1>
-        <a href="/api/auth/logout">Logout</a>
-      </>
+      <div className={liStyles.container}>
+        <h1>Logged In</h1>
+        <a href="api/auth/logout">Log out</a>
+        <Dashboard />
+      </div>
     );
   }
 
-  return (
-    <div className={styles.landingDiv}>
-      <Image className={""} src={Logo} alt="Gregson Lane Cricket Club Logo" />
-      <a href="api/auth/login">
-        <button>Log In</button>
-      </a>
-      {/* <div className={""} />
-      <a href="api/auth/signup">
-        <button>Register</button>
-      </a> */}
-    </div>
-  );
+  if (!user) {
+    return (
+      <div className={loStyles.loggedOut}>
+        <div className={loStyles.divLogin}>
+          <Image
+            className={loStyles.logo}
+            src={imgLogo}
+            alt="Gregson Lane Cricket Club Logo"
+          />
+          <a href="api/auth/login">
+            <button className={loStyles.button}>
+              <span className={loStyles.btnText}>Log In</span>
+              <Image
+                className={loStyles.btnIcon}
+                src={imgLogin}
+                alt="Log in icon"
+              />
+            </button>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
