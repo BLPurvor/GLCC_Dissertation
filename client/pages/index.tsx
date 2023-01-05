ab18847b8c8e8c3ee7ff497f20dd4a08ee78c581
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-01-05 23:38:49 +0000

    Update index.tsx
    Changed a tags to Link tags for better NextJS support.
    
    Added Header element to logged in page.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import loStyles from "../styles/Landing.module.scss";
 import liStyles from "../styles/Home.module.scss";
 
-import Header from "../components/";
+import Header from "../components/Header";
 
 import imgLogo from "../assets/brand/logo.png";
 import imgLogin from "../assets/shared/login.svg";
 import imgLoading from "../assets/shared/loading.svg";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
+import Nav from "../components/Nav";
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
@@ -37,9 +39,9 @@ export default function Home() {
   if (user) {
     return (
       <div className={liStyles.container}>
+        <Header />
         <h1>Logged In</h1>
-        <a href="api/auth/logout">Log out</a>
-        <Dashboard />
+        <Link href="api/auth/logout">Log out</Link>
       </div>
     );
   }
@@ -53,7 +55,7 @@ export default function Home() {
             src={imgLogo}
             alt="Gregson Lane Cricket Club Logo"
           />
-          <a href="api/auth/login">
+          <Link href="api/auth/login">
             <button className={loStyles.button}>
               <span className={loStyles.btnText}>Log In</span>
               <Image
@@ -62,7 +64,7 @@ export default function Home() {
                 alt="Log in icon"
               />
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     );
