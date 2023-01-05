9b7410259fd2ccd55184f8de56dcb7c6832f535a
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-01-05 23:37:20 +0000

    Created Header
    Created and styled header component to be called on each page used after the user has logged in. Should allow for navigation and access to user account options.

---
+++ client/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import imgLogo from "../../assets/brand/logo.png";
+import imgUser from "../../assets/shared/user.svg";
+import Nav from "../Nav";
+
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
+  useEffect(() => {
+    console.log(burgerStatus);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapBurger}>
+        <button
+          className={styles.iconBurger}
+          type="button"
+          onClick={() => setBurgerStatus(!burgerStatus)}
+        >
+          <div className={styles.iconB1}></div>
+          <div className={styles.iconB2}></div>
+          <div className={styles.iconB3}></div>
+        </button>
+        <Nav active={burgerStatus} />
+      </div>
+      <Link href="/">
+        <Image
+          className={styles.imgLogo}
+          src={imgLogo}
+          alt="Gregson Lane CC Logo"
+        />
+      </Link>
+      <Link href="/account">
+        <Image
+          className={styles.iconUser}
+          src={imgUser}
+          alt="Account Options Icon"
+        />
+      </Link>
+    </div>
+  );
+}
