4b26bbd682f2ceba182f7e6c315a4c9a23d63b8a
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-01-16 22:44:36 +0000

    Update ABOUT
    Added some styling for the shape of articles within the About page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -2,8 +2,9 @@ import Loading from "../components/Loading";
 import Layout from "../components/Layout";
 
 import styles from "../styles/About.module.scss";
+import twos_2022 from "../assets/gallery/twos/team_photo_2022.png";
+
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Router from "next/router";
 import Image from "next/image";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
@@ -34,14 +35,35 @@ export default function About() {
             The 1<sup>st</sup> XI
           </h2>
           {/* <Image */}
-          <p></p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            sollicitudin ante consectetur commodo eleifend. Suspendisse urna
+            libero, tincidunt tincidunt dui non, pretium auctor odio.
+            <br />
+            Nunc enim neque, lobortis sed consequat at, posuere eget orci. In a
+            nisi quam. Aenean convallis porta turpis maximus porttitor. Nunc
+            rutrum, lectus nec faucibus condimentum, quam magna interdum erat,
+            at maximus metus nibh non odio.
+          </p>
         </article>
         <article id="SecondTeam">
           <h2 className={styles.artHeader}>
             The 2<sup>nd</sup> XI
           </h2>
-          {/* <Image */}
-          <p></p>
+          <Image
+            src={twos_2022}
+            alt="Gregson Lane Second Team 2021/22 Season"
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            sollicitudin ante consectetur commodo eleifend. Suspendisse urna
+            libero, tincidunt tincidunt dui non, pretium auctor odio.
+            <br />
+            Nunc enim neque, lobortis sed consequat at, posuere eget orci. In a
+            nisi quam. Aenean convallis porta turpis maximus porttitor. Nunc
+            rutrum, lectus nec faucibus condimentum, quam magna interdum erat,
+            at maximus metus nibh non odio.
+          </p>
         </article>
         <article id="SundayTeam">
           <h2>The Sunday XI</h2>
@@ -52,7 +74,8 @@ export default function About() {
             teams.
             <br />
             Acting as a gateway for players from juniors to those who have just
-            picked the game up, the Sunday team welcomes players of any ability.
+            picked the game up, the Sunday team welcomes players of any ability
+            - if you can stand up to a bit of sledging!
           </p>
         </article>
       </div>
