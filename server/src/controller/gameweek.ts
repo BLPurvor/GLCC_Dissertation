4b8916a7b66d90d32dba7ad68d41e07d6ac162fc
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-01-23 23:21:41 +0000

    Update Gameweek Controller
    Added create function. Not complete

--- server/src/controller/gameweek.ts
+++ server/src/controller/gameweek.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { prisma } from "../db";
 
 type Gameweek = {
@@ -25,11 +26,37 @@ export const getAllStd = async (): Promise<Gameweek[]> => {
 };
 
 export const getById = async (id: string): Promise<Gameweek | null> => {
-  const result = prisma.gameweek.findFirst({
+  const result = await prisma.gameweek.findFirst({
     where: {
       id: id,
     },
   });
 
   return result;
+};
+
+export const create = async (
+  user: string,
+  matches: string
+): Promise<String> => {
+  const lastPrize = await prisma.gameweek.findFirst({
+    select: { prize: true },
+    take: -1,
+  });
+
+  if (!lastPrize) return "EFGWLP";
+
+  let newPrize = lastPrize.prize + 25;
+
+  const query = await prisma.gameweek.create({
+    data: {
+      authorName: user,
+      prize: newPrize,
+      matches: matches,
+    },
+  });
+
+  if (!query) return "ECGWQU";
+
+  return "Success";
 };
