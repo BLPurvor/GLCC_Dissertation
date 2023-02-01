ebcc424d54460477e6302b7931616b6d36ea2337
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-01 12:20:26 +0000

    Created getBetweenDates Function
    Created function to request from API-Football based on given URL parameters.

--- server/src/controller/matches.ts
+++ server/src/controller/matches.ts
@@ -1,15 +1,47 @@
+import axios from "axios";
+import { config } from "dotenv";
+
 const Leagues = {
   engDiv1: "39",
   engDiv2: "40",
 };
 
-export const getBetweenDates = (
-  league: number,
-  season: number,
-  startDate: Date,
-  endDate: Date
-) => {
-  const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${league}&season=${season}&from=${startDate}&to=${endDate}`;
+type urlOptions = {
+  season: number;
+  startDate: string;
+  endDate: string;
+};
 
-  // Fetch all data fixtures between given date to pass to Next.JS front-end selector for volunteers/admins
+// Fetch all data fixtures between given date to pass to Next.JS front-end selector for volunteers/admins
+export const getBetweenDates = async (
+  league: number,
+  urlOptions: urlOptions
+): Promise<any> => {
+  const options = {
+    url: "https://api-football-v1.p.rapidapi.com/v3/fixtures",
+    params: {
+      league: league,
+      season: urlOptions.season,
+      from: urlOptions.startDate,
+      to: urlOptions.endDate,
+    },
+    headers: {
+      "X-RapidAPI-Key": process.env.FBAPI_KEY,
+      "X-RapidAPI-Host": process.env.FBAPI_HOST,
+    },
+  };
+
+  let result = axios
+    .request(options)
+    .then((response) => {
+      if (Object.keys(response.data.errors).length > 0) {
+        return response.data.errors;
+      }
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
+
+  return result;
 };
