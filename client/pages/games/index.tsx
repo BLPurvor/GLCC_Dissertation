e2ade35bc34e778d3b441525d5ab8a60e7ce16d3
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-06 17:42:15 +0000

    Games Navigation
    Created index page to host game options. Restructured files to use existing games.tsx as index.tsx within games folder for better routing.

--- client/pages/games/index.tsx
+++ client/pages/games/index.tsx
@@ -1,22 +1,25 @@
-import Loading from "../components/Loading";
-import Layout from "../components/Layout";
+import Loading from "../../components/Loading";
+import Layout from "../../components/Layout";
 
-import styles from "../styles/Games.module.scss";
+import styles from "../../styles/Games.module.scss";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { userInfoFetch, walletInfoFetch } from "../../scripts/userInfo";
+import useSWR from "swr";
 
 export const getServerSideProps = withPageAuthRequired();
 
 export default function Games() {
   const { user, isLoading } = useUser();
+  const user_id: string = user!.sub!.substring(user!.sub!.indexOf("|") + 1);
 
   if (isLoading) {
     return <Loading />;
   }
 
   return (
-    <Layout>
+    <Layout user_id={user_id}>
       <div className={styles.container}>
         <h1 className={styles.txtHeader}>Games</h1>
         <div className={styles.gamesHousing}>
