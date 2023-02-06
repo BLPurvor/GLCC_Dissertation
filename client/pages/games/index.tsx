b095139680f8449ddaf9898e73c10fecdb77227e
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-06 22:44:28 +0000

    Filled out Game Page
    Filled out game page and removed link functionality for placeholder button. Added styling elements and navigation using grid-layout buttons for existing games

--- client/pages/games/index.tsx
+++ client/pages/games/index.tsx
@@ -1,33 +1,46 @@
 import Loading from "../../components/Loading";
 import Layout from "../../components/Layout";
 
-import styles from "../../styles/Games.module.scss";
+import styles from "../../styles/games/index.module.scss";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { userInfoFetch, walletInfoFetch } from "../../scripts/userInfo";
 import useSWR from "swr";
+import Link from "next/link";
 
 export const getServerSideProps = withPageAuthRequired();
 
 export default function Games() {
   const { user, isLoading } = useUser();
-  const user_id: string = user!.sub!.substring(user!.sub!.indexOf("|") + 1);
 
   if (isLoading) {
     return <Loading />;
   }
+
+  const user_id: string = user!.sub!.substring(user!.sub!.indexOf("|") + 1);
 
   return (
     <Layout user_id={user_id}>
       <div className={styles.container}>
         <h1 className={styles.txtHeader}>Games</h1>
         <div className={styles.gamesHousing}>
-          <a href="/games/coupon">
-            <button className={styles.gameButton} id={styles.coupon}>
-              Coupon
+          <Link href="/games/coupon">
+            <button
+              className={`${styles.gameButton} ${styles.btnOdd}`}
+              id={styles.coupon}
+            >
+              <span>Predictor</span>
             </button>
-          </a>
+          </Link>
+          {/* <Link href=""> */}
+          <button
+            className={`${styles.gameButton} ${styles.btnEven}`}
+            id={styles.placeholder}
+          >
+            <span>Fantasy League</span>
+          </button>
+          {/* </Link> */}
         </div>
       </div>
     </Layout>
