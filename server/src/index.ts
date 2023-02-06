62420e11015fb4b807a6631c5ad29a6233cf9af7
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-06 17:44:40 +0000

    Overall Server Update
    Fixed CORS error (added all origins)
    Removed unused import
    Added routes for User and Wallet requests.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,26 @@
 import express, { Express, Request, Response } from "express";
-import config from "./config";
+import cors from "cors";
 
+import { routeUser } from "./routes/user";
 import { routeGameweek } from "./routes/gameweek";
 import { routeMatches } from "./routes/matches";
-
-import { prisma } from "./db";
+import { routeWallet } from "./routes/wallet";
 
 const PORT = process.env.APP_PORT;
 
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req: Request, res: Response) => {
   return res.status(200).send("Root");
 });
 
+app.use("/user/", routeUser);
 app.use("/gameweek/", routeGameweek);
 app.use("/matches/", routeMatches);
+app.use("/wallet", routeWallet);
 
 try {
   app.listen(PORT, () => {
