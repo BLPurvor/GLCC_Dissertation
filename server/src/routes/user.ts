5c2cfd633bf1c15ef5d484a0a927a3eb89cfcea2
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-26 19:34:17 +0000

    Improved error handling of getbyID

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -13,7 +13,26 @@ router.post("/update", async (req: Request, res: Response) => {
   // Update user details based on user_id given in URL
   const result = await updateById(req.body);
 
-  return res.send(result);
+  switch (result) {
+    case "E00000":
+      return res.status(200).send("Successfully updated details!");
+    case "EUUPAS":
+      return res
+        .status(401)
+        .send(
+          "Could not update using the provided credentials. Please try again."
+        );
+    case "EUDIPS":
+      return res
+        .status(501)
+        .send(
+          "Something went wrong. Please try again later. If this error persists, please contact the administrator."
+        );
+    case "EGUIPF":
+      return res.status(400).send("Please provide your password.");
+    default:
+      return res.status(500).send(result);
+  }
 });
 
 export { router as routeUser };
