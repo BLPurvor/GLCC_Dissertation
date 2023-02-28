9e1247439c1d439f94220a9c0281a4b0137741e5
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-02-28 16:50:06 +0000

    Update new.tsx
    Progressive update to new coupon page.
    
    Added counter to track x/8 selected matches. Need to reconsider how matches are fetched. Could do with combining arrays and getting both leagues at the same time regardless of user input.

--- client/pages/volunteer/coupon/new.tsx
+++ client/pages/volunteer/coupon/new.tsx
@@ -18,7 +18,8 @@ export const getServerSideProps = withPageAuthRequired(); // Force the user to b
 
 export default function NewCoupon() {
   const { user, isLoading } = useUser(); // Get current user information.
-  const [fixtures, setFixtures] = useState(Array<Fixture>); // Fixtures array defined through state for conditional hook
+  const [fixtures, setFixtures] = useState(Array<Fixture>); // Fixtures array defined through state for conditional rendering of fixture checkboxes.
+  const [selection, setSelection] = useState(Array<Element>);
   const [postError, setPostError] = useState("");
 
   if (!user || !user.sub) return <Custom404 />; // If user isn't logged in, or if the sub value - which is used for identification - isn't defined, send the user to the error page.
@@ -29,7 +30,7 @@ export default function NewCoupon() {
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault(); // Stop form submitting post/get request to the page upon submission.
 
-    const form = document.querySelector("#dateSelect")! as HTMLFormElement; // Form with ID = "dateSelect" is defined on page load. ! operator forces program to consider it as non-nullable.
+    const form = document.querySelector("#dateSelect") as HTMLFormElement; // Form with ID = "dateSelect" is defined on page load. ! operator forces program to consider it as non-nullable.
 
     const url = `http://localhost:3001/matches/between/2022/${form.startDate.value}/${form.endDate.value}/${form.league.value}`; // Concatenate values from the form fields when the query is run. Season not a requirement at this stage of development
 
@@ -62,7 +63,7 @@ export default function NewCoupon() {
 
   function handleRender(fixtures: Fixture[] | string) {
     // Check if the passed prop is a string, i.e. If an error code is passed back from axios response
-    if (typeof fixtures === "string") {
+    if (typeof fixtures === "string" || fixtures === undefined) {
       return (
         <div className={styles.errContainer}>
           <h1>Something went wrong.</h1>
@@ -130,6 +131,14 @@ export default function NewCoupon() {
     });
   }
 
+  function handleUpdate() {
+    let checkedElements = document.querySelectorAll(
+      "input[type='checkbox']:checked"
+    );
+
+    setSelection(Array.from(checkedElements));
+  }
+
   return (
     <Layout user_id={user_id}>
       <div className={styles.container}>
@@ -163,13 +172,7 @@ export default function NewCoupon() {
           </div>
           <div className={styles.leagueHousing}>
             <label htmlFor="leaguePL" className={styles.leagueSelector}>
-              <input
-                required
-                type="radio"
-                id="leaguePL"
-                name="league"
-                value="39"
-              />
+              <input type="radio" id="leaguePL" name="league" value="39" />
               <div className={styles.leagueSelectHousing}>
                 <Image
                   width={50}
@@ -181,13 +184,7 @@ export default function NewCoupon() {
               </div>
             </label>
             <label htmlFor="leagueCH" className={styles.leagueSelector}>
-              <input
-                required
-                type="radio"
-                id="leagueCH"
-                name="league"
-                value="40"
-              />
+              <input type="radio" id="leagueCH" name="league" value="40" />
               <div className={styles.leagueSelectHousing}>
                 <Image
                   width={50}
@@ -199,79 +196,33 @@ export default function NewCoupon() {
               </div>
             </label>
           </div>
-          <input type="submit" className={styles.dateSubmit} />
-        </form>
-        <form id="fixtureSelect" onSubmit={handleCreation}>
           <input
+            type="submit"
+            className={styles.dateSubmit}
+            value="Get Fixtures"
+          />
+        </form>
+        <div
+          className={`${styles.counter} 
+          ${selection.length === 0 ? styles.invisible : ""} 
+          ${selection.length < 8 ? styles.invalidMatches : ""}`}
+        >
+          <span className={styles.counterxt}>{selection.length}/8 Matches</span>
+        </div>
+        <form
+          className={styles.fixtureHousing}
+          id="fixtureSelect"
+          onSubmit={handleCreation}
+          onChange={handleUpdate}
+        >
+          <input
+            className={styles.matchweekSubmit}
+            disabled={selection.length < 8}
             hidden={fixtures.length < 1}
             type="submit"
             value="Create Matchweek"
           />
           {fixtures !== null ? handleRender(fixtures) : handleRender("Null")}
-          {/* {fixtures ? (
-            fixtures.map((fixture) => {
-              return (
-                <label
-                  key={fixture.fixture.id}
-                  htmlFor={fixture.fixture.id.toString()}
-                  className={styles.fixture}
-                >
-                  <input
-                    type="checkbox"
-                    id={fixture.fixture.id.toString()}
-                    name={fixture.fixture.id.toString()}
-                    value={fixture.fixture.id}
-                  />
-                  <div className={styles.housing}>
-                    <div className={styles.fixtureInfo}>
-                      <p className={styles.date}>
-                        {new Intl.DateTimeFormat("en-GB", {
-                          hour: "2-digit",
-                          minute: "2-digit",
-                          weekday: "long",
-                          day: "numeric",
-                          month: "long",
-                        }).format(new Date(fixture.fixture.date))}
-                      </p>
-                      <p className={styles.venue}>
-                        {fixture.fixture.venue.name},{" "}
-                        {fixture.fixture.venue.city}
-                      </p>
-                    </div>
-                    <div className={styles.mainContainer}>
-                      <div className={styles.team}>
-                        <img
-                          src={`${fixture.teams.home.logo}`}
-                          alt={fixture.teams.home.name}
-                        />
-                        <h1>{fixture.teams.home.name}</h1>
-                      </div>
-
-                      <p className={styles.matchSeparator}>v</p>
-
-                      <div className={styles.team}>
-                        <img
-                          src={`${fixture.teams.away.logo}`}
-                          alt={fixture.teams.away.name}
-                        />
-                        <h1>{fixture.teams.away.name}</h1>
-                      </div>
-                    </div>
-                  </div>
-                </label>
-              );
-            })
-          ) : (
-            <div className={styles.errContainer}>
-              <h1>Something went wrong.</h1>
-              <p>
-                We aren't sure what has happened, but something has gone wrong.
-                Please try again later.
-                <br />
-                Please contact the administrator if this error persists.
-              </p>
-            </div>
-          )} */}
         </form>
       </div>
     </Layout>
