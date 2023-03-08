f686f13f8e8f184107f738899c7d3a69c5da2496
BLPurvor <80334444+BLPurvor@users.noreply.github.com>
2023-03-08 12:17:31 +0000

    Added types to backend

---
+++ server/src/types/user.ts
@@ -0,0 +1,13 @@
+export type User = {
+  _id?: string; // uuid generated on signup.
+  email?: string;
+  email_verified?: boolean; // Has the user followed the email to verify their email.
+  username?: string;
+  password?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  wallet_id?: string; // uuid generated on signup and creation of the wallet property.
+  role?: string; // Can only be specified strings.
+  first_name?: string;
+  last_name?: string;
+};
